44041893653cdef375c42dddcca8cee42d24f0e9
freeman <the.freeman.lab@gmail.com>
2015-08-09 17:12:37 -0400

    Refactor to extend matrix

--- src/index.js
+++ src/index.js
@@ -1,181 +1,139 @@
 'use strict';
-var LightningVisualization = require('lightning-visualization');
+var Matrix = require('lightning-matrix');
 var d3 = require('d3');
 var utils = require('lightning-client-utils');
 var _ = require('lodash');
-var Color = require('color');
-var colorbrewer = require('colorbrewer')
 
-/*
- * Extend the base visualization object
- */
-var Visualization = LightningVisualization.extend({
+var Visualization = Matrix.extend({
 
-    init: function() {
-        this.render();  
-    },
-
-    render: function() {
-        var width = this.width
-        var height = this.height
-        var data = this.data
-        var selector = this.selector
-        var self = this
-
-        var entries = data.entries
-        var nrow = data.nrow
-        var ncol = data.ncol
-        var label = data.label
-
-        // automatically scale stroke width by number of cells
-        var strokeWidth = Math.max(1 - 0.00009 * nrow * ncol, 0.1);
-
-        // get min and max of matrix value data
-        var zmin = d3.min(entries, function(d) {
-            return d.z
-        });
-        var zmax = d3.max(entries, function(d) {
-            return d.z
-        });
-
-        // set up x and y scales and ranges
-        var y = d3.scale.ordinal().rangeBands([0, Math.min(width, height)]).domain(d3.range(nrow));
-        var x = d3.scale.ordinal().rangeBands([0, Math.min(width, height)]).domain(d3.range(ncol));
-        var maxY = nrow * y.rangeBand();
-        var maxX = ncol * x.rangeBand();
-
-        // sort by label
-        y.domain(d3.range(nrow).sort(function(a, b) { return label[a] - label[b]; }));
-        x.domain(d3.range(ncol).sort(function(a, b) { return label[a] - label[b]; }))
-
-        // set up colors
-        var color = utils.getColors(_.uniq(label).length);
-
-        // set up opacity scale
-        var zdomain = [0, zmax]
-        var z = d3.scale.linear().domain(zdomain).range([0.3,1]).clamp(true);
-
-        // set up variables to toggle with keypresses
-        var scale = 0
-
-        // bounds for the graphic
-        var bounds = [[0, 0], [maxY, maxX]];
-
-        // create canvas
-        var canvas = d3.select(selector)
-            .append('canvas')
-            .attr('width', width)
-            .attr('height', height)
-            .node().getContext("2d")
-
-        // create dummy container for data binding
-        var detachedContainer = document.createElement("custom");
-        var dataContainer = d3.select(detachedContainer);
-
-        // drawing wrapper to handle binding
-        function drawCustom(data) {
-
-            var dataBinding = dataContainer.selectAll("custom.rect")
-                .data(data);
-
-            dataBinding
-                .attr("fillStyle", function(d) {return d.z > 0 ? utils.buildRGBA(d.c, z(d.z)) : "#eee"});
-              
-            dataBinding.enter()
-                .append("custom")
-                .classed("rect", true)
-                .attr("x", function(d) {return x(d.x)})
-                .attr("y", function(d, i) {return y(d.y)})
-                .attr("width", y.rangeBand())
-                .attr("height", x.rangeBand())
-                .attr("fillStyle", function(d) {return (d.z > 0) ? utils.buildRGBA(d.c, z(d.z)) : "#eee"})
-                .attr("strokeStyle", "white")
-                .attr("lineWidth", strokeWidth)
-      
-            drawCanvas();
-        
+    getDefaultOptions: function() {
+        return {
+            labels: false,
+            symmetric: true,
+            sort: "label"
         }
-
-        // draw the matrix
-        function drawCanvas() {
-
-          // clear canvas
-          canvas.clearRect(0, 0, width, height);
-          
-          // select nodes and draw their data to canvas
-          var elements = dataContainer.selectAll("custom.rect");
-          elements.each(function(d) {
-            var node = d3.select(this);
-            canvas.beginPath();
-            canvas.fillStyle = node.attr("fillStyle");
-            canvas.strokeStyle = node.attr("strokeStyle");
-            canvas.lineWidth = node.attr("lineWidth");
-            canvas.rect(node.attr("x"), node.attr("y"), node.attr("height"), node.attr("width"));
-            canvas.fill();
-            canvas.stroke();
-            canvas.closePath();
-          })
-        }
-
-        // add keydown events
-        d3.select(selector).attr('tabindex', -1)
-        d3.select(selector).on('keydown', update)
-
-        function update() {
-            if (d3.event.keyCode == 38 | d3.event.keyCode == 40) {
-                d3.event.preventDefault();
-                if (d3.event.keyCode == 38) {
-                    scale = scale + 0.1
-                    if (scale > 0.95) {
-                        scale = 0.95
-                    }
-                }
-                if (d3.event.keyCode == 40) {
-                    scale = scale - 0.1
-                    if (scale < -1) {
-                        scale = -1
-                    }
-                }
-                console.log(zmax - zmax * scale)
-                zdomain = [0, zmax - zmax * scale]
-                z.domain(zdomain)
-                drawCustom(entries);
-            }
-        }
-
-        drawCustom(entries)
     },
 
     formatData: function(data) {
         var matrix = [];
-        var n = data.nodes.length
-        var label = data.label ? data.label : _.times(n, _.constant(0));
-        var min = d3.min(label);
-        label = label.map(function(d) {return d - min});
-        var color = utils.getColors(_.uniq(label).length);
+        var self = this;
 
+        // parse labels
+        var n = data.nodes.length;
+        var label = data.label ? data.label : _.times(n, _.constant(0));
+        label = label.map(function(d) {return d - d3.min(label)});
+
+        // fill matrix with node and link info
         data.nodes.forEach(function(node, i) {
             matrix[i] = d3.range(n).map(function(j) { return {x: j, y: i, z: 0, c: "black"}; });
         });
         data.links.forEach(function(link) {
             matrix[link[0]][link[1]].z = link[2];
             if (label[link[0]] == label[link[1]]) {
-                matrix[link[0]][link[1]].c = color[label[link[0]]];
+                matrix[link[0]][link[1]].l = label[link[0]];
+            } else {
+                matrix[link[0]][link[1]].l = -1;
             }
         });
-        var zMin = d3.min(data.links, function(d) {return d[2]})
-        console.log(zMin)
-        d3.range(n).forEach(function(i) { 
-            matrix[i][i].z = zMin
-            matrix[i][i].c = color[label[i]]
-        })
 
-        var entries = _.flatten(matrix)
+        // add extra entries to symmetrize
+        if (self.options.symmetric) {
+            data.links.forEach(function(link) {
+                matrix[link[1]][link[0]].z = link[2];
+                if (label[link[0]] == label[link[1]]) {
+                    matrix[link[1]][link[0]].l = label[link[0]];
+                } else {
+                    matrix[link[1]][link[0]].l = -1;
+                }
+            });
+        }
 
-        var nrow = matrix.length
-        var ncol = matrix[0].length
+        // fill in diagnonals
+        d3.range(n).forEach(function(i) {
+            matrix[i][i].z = zmin;
+            matrix[i][i].l = label[i];
+        });
+        var entries = _.flatten(matrix);
 
-        return {entries: entries, nrow: nrow, ncol: ncol, label: label}
+        // get summary statistics
+        var nrow = matrix.length;
+        var ncol = matrix[0].length;
+        var zmin = d3.min(data.links, function(d) {return d[2]});
+        var zmax = d3.max(data.links, function(d) {return d[2]});
+        var rowsum = _.map(_.range(nrow), function(i) {
+            return d3.sum(_.filter(entries, function(d) {return d.y == i}), function(d) {return d.z})
+        });
+        var colsum = _.map(_.range(ncol), function(j) {
+            return d3.sum(_.filter(entries, function(d) {return d.x == j}), function(d) {return d.z})
+        });
+
+        return {entries: entries, nrow: nrow, ncol: ncol, rowsum: rowsum, colsum: colsum,
+            zmin: zmin, zmax: zmax, label: label, rows: data.names, columns: data.names}
+    },
+
+    sortRows: function() {
+        var self = this;
+        if (this.options.sort == 'label') {
+            var sorter = function(a, b) {
+                return self.data.label[b] - self.data.label[a]
+            };
+            this.y.domain(d3.range(this.data.nrow).sort(sorter));
+            this.x.domain(d3.range(this.data.ncol).sort(sorter));
+        }
+        if (this.options.sort == 'degree') {
+            var colsorter = function(a, b) {
+                return self.data.colsum[b] - self.data.colsum[a]
+            };
+            var rowsorter = function(a, b) {
+                return self.data.colsum[b] - self.data.colsum[a]
+            };
+            this.y.domain(d3.range(this.data.nrow).sort(rowsorter));
+            this.x.domain(d3.range(this.data.ncol).sort(colsorter));
+        }
+    },
+
+    makeScales: function() {
+        var self = this;
+        var n = _.uniq(this.data.label).length;
+        var color = utils.getColors(n);
+        this.c = d3.scale.ordinal();
+        this.c.domain([0, n])
+            .range(color);
+        this.z = d3.scale.linear();
+        this.z.domain(utils.linspace(self.data.zmin, self.data.zmax, 9))
+            .range([0.3, 1]).clamp(true)
+    },
+
+    updateRange: function(d) {
+        this.c.range(colorbrewer[d][9])
+    },
+
+    updateDomain: function(d) {
+        this.z.domain(d)
+    },
+
+    formatLabel: function(val) {
+        if (val == 0 || isNaN(val)) {
+            return '';
+        } else {
+            return parseFloat(d3.format(".2f")(val)).toString();
+        }
+    },
+
+    getColor: function(d, opacity) {
+        var color;
+        if (d.l == -1) {
+            color = 'rgb(150,150,150)';
+        } else {
+            color = this.c(d.l);
+        }
+        if (d.z == 0) {
+            color =  "#eee";
+        }
+        if (opacity < 1.0) {
+            return utils.buildRGBA(color, 0.15);
+        }
+        return (d.z != 0) ? utils.buildRGBA(color, this.z(d.z) * opacity) : "#eee"
     }
 
 });
